6488a153606e1eabe3480e17e2a3e694a9743b0b
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-23 16:07:49 +0900

    원핸드게임

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args){ 
         String name = "Angela";
         System.out.println(name);
+        name = "Song";
+        System.out.println();
        // return 0;
     }
 }
---
+++ src/MakingGamebyOneHand.java
@@ -0,0 +1,17 @@
+public class MakingGamebyOneHand {
+    public static void main(String[] args) {
+        String A = "";
+        String B = "";
+        String C = "";
+       A = "1";
+       C = "2";
+       B = A ; 
+       A = C ; 
+       System.out.println(A);
+       System.out.println(B);
+       System.out.println(C);
+         // return 0;
+    
+    }
+    
+}
