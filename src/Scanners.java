aebfa6b3fe5d10518c59087327551813a843cbef
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-24 13:37:53 +0900

    aaa

--- src/Scanners.java
+++ src/Scanners.java
@@ -1,4 +1,6 @@
  import java.util.Scanner;
+
+import javax.management.ServiceNotFoundException;
  public class Scanners {public static void main(String[] args) {
    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 //     System.out.println("Enter username");
@@ -6,15 +8,15 @@
 //     String userName = myObj.nextLine();  // Read user input
 //     System.out.println("Username is: " + userName);  // Output user input
 
-    int A = 0;
-    int B = 0;
+    int First = 0;
+    int Second = 0;
    
 
-    A = myObj.nextInt(); 
-    B = myObj.nextInt();
+    First = myObj.nextInt(); 
+   Second = myObj.nextInt();
 
 
-    System.out.println(A + B);
+    System.out.println(First  + Second);
 
     //return 0;
 }
