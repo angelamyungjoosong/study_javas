540e32183a10602aee8a17b8988f1727baa50244
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-26 17:53:34 +0900

    ss

--- src/cases/CashierFor.java
+++ src/cases/CashierFor.java
@@ -17,7 +17,7 @@ public class CashierFor {
              System.out.println();
             System.out.println("--- 담은 총합 ---");
             System.out.println("합계:" + (bucket[0]+bucket[1]+bucket[2]+bucket[3]+bucket[4]));
-            System.out.println();2
+            System.out.println();
             System.out.println("--- 계산 종료 ---");
             
         } catch(
@@ -31,21 +31,22 @@ public class CashierFor {
     }
 
     System.out.println();
-}}
-
+}
+}
 
 // int bucket []= {0,0,0,0,0};
 // System.out.println("--- 계산 시작 ---");
 
 // Scanner myObj = new Scanner(System.in);
 // for (int first=0; bucket[first] < 5 ; first=first+1){
-//  if (bucket[first]<= 100){System.out.print((first+1)+"번째 가격 : ");
-//  bucket[first]= myObj.nextInt();}
-//  else if (bucket[first]>100){ System.out.print((first+1)+"번째 가격 : "); 
-//  bucket[first]= myObj.nextInt();bucket[first]=0;}
-//  else if (bucket[first]==0){System.out.print((first+1)+"번째 가격 : ");
+// if (bucket[first]<= 100){System.out.print((first+1)+"번째 가격 : ");
+// bucket[first]= myObj.nextInt();}
+// else if (bucket[first]>100){ System.out.print((first+1)+"번째 가격 : ");
+// bucket[first]= myObj.nextInt();bucket[first]=0;}
+// else if (bucket[first]==0){System.out.print((first+1)+"번째 가격 : ");
 // bucket[first]= myObj.nextInt();break;} }
 
 // System.out.println("--- 담은 총합 ---");
-// System.out.print("합계:" + (bucket[0]+bucket[1]+bucket[2]+bucket[3]+bucket[4]));
+// System.out.print("합계:" +
+// (bucket[0]+bucket[1]+bucket[2]+bucket[3]+bucket[4]));
 // System.out.println("--- 계산 종료 ---");
