c51c6b5c9cd1a53d4cf45f85b1a9874ab0679fb0
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-26 18:10:14 +0900

    yay

--- src/cases/CashierFor.java
+++ src/cases/CashierFor.java
@@ -10,16 +10,17 @@ public class CashierFor {
             System.out.println("--- 계산 시작 ---");
 
             Scanner myObj = new Scanner(System.in);
-            for (int first=0; bucket[first] < 5 ; first=first+1){System.out.print((first+1)+"번째 가격 : ");
+            for (int first=0; bucket[first] < 5 ; first=first+1)
+            {System.out.print((first+1)+"번째 가격 : ");
             bucket[first]= myObj.nextInt();
              if (bucket[first]>100){ bucket[first]=0;}
              else if (bucket[first]==0){break;} }
-             System.out.println();
+            System.out.println();
             System.out.println("--- 담은 총합 ---");
             System.out.println("합계:" + (bucket[0]+bucket[1]+bucket[2]+bucket[3]+bucket[4]));
             System.out.println();
             System.out.println("--- 계산 종료 ---");
-            
+        
         } catch(
 
     Exception e)
