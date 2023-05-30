a3656eebac43e212779e6d94018821d30017ced2
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-30 17:19:19 +0900

    s

--- src/Methods.java
+++ src/Methods.java
@@ -1,45 +1,91 @@
+// public class Methods {
+//     // <access_modifier> <return_type> <method_name>( list_of_parameters)
+//     // {
+//     // //body
+//     // }
+
+//     public int minus(int first, int second) {
+//         int result = 0;
+//         try {
+//             result = first - second;
+//         } catch (Exception e) {
+//             // TODO: handle exception
+//         }
+//         return result;
+//     }
+
+//     public int add(int first, int second) {
+
+//         int result = 0;
+//         try {
+//             result = first + second;
+
+//         } catch (Exception e) {
+//             // TODO: handle exception
+//         }
+//         return result;
+
+//     }
+//         public static int main(int first, int second) {
+//             //인스턴스 1회
+//             //초기 값은 2를 넘지 않음 
+//             //main 결과값은 17
+//             int result = 0;
+            
+//             try {first = 2;
+//                 second = 1;
+//                 Methods methods = new Methods();
+//                 result = methods.add(first, first);
+//                 result = methods.add(first, first);
+//                 result = methods.add(first, first);
+//                 result = methods.add(first, second);
+    
+//             } catch (Exception e) {
+//                 // TODO: handle exception
+//             }
+           
+//             return result;
+            
+//     }
+// }
+
 public class Methods {
     // <access_modifier> <return_type> <method_name>( list_of_parameters)
     // {
     // //body
     // }
-
-    public int minus(int first, int second) {
+    public int minus(int first, int second){
         int result = 0;
         try {
-            result = first - second;
+           result = first - second;
         } catch (Exception e) {
             // TODO: handle exception
         }
         return result;
     }
-
-    public int add(int first, int second) {
-
+    public int add(int first, int second){
         int result = 0;
         try {
             result = first + second;
-
         } catch (Exception e) {
             // TODO: handle exception
         }
-        return result;
+        return result ;
     }
-
+    
     public int main(int first, int second) {
-        //인스턴스 1회
-        //초기 값은 2를 넘지 않음 
-        //main 결과값은 17
+        // - 인스턴스는 1회
+        // - 초기 값은 2를 넘지 않음.
+        // - main 결과값은 17
         int result = 0;
-        
-        try {first = 2;
-            second = 1;
-            Methods methods = new Methods();
-            result = methods.add(first, first);
-            result = methods.add(first, first);
-            result = methods.add(first, first);
-            result = methods.add(first, second);
-
+            try {first = 2;
+                second = 1;
+                Methods methods = new Methods();
+                result = methods.add(first, first);
+                result = methods.add(first, first);
+                result = methods.add(first, first);
+                result = methods.add(first, second);
+    
         } catch (Exception e) {
             // TODO: handle exception
         }
---
+++ src/Templates.java
@@ -0,0 +1,12 @@
+public class Templates {public static void main(String[] args) {
+    try {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+    System.out.println();
+    
+    //return 0;
+}
+    
+}
