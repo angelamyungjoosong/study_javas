d97bcb14e359b9c3a24e6bef09d053425e72c0ea
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-30 19:12:53 +0900

    done

--- src/MethodsIfs.java
+++ src/MethodsIfs.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;//class
 public class MethodsIfs 
 {public String statement(int time) {
-    time = 15;
+    
     String statement="";
    
     try {
@@ -20,11 +21,14 @@ statement= "오전입니다";
 }  
 
 public static void main(String[] args) {
-try {int time = 15;
-    
-    MethodsIfs methodifs = new MethodsIfs();
+try {int time = 0;
+    Scanner myObj= new Scanner(System.in);
+
+    while (time <25){
+        MethodsIfs methodifs = new MethodsIfs();
+    time= myObj.nextInt();
     String returnresult = methodifs.statement(time);
-    System.out.println(returnresult);
+    System.out.println(returnresult);}
 
 } catch (Exception e) {
     // TODO: handle exception
@@ -33,3 +37,40 @@ try {int time = 15;
 }
 
 }
+
+
+// public class MethodsIfs 
+// {public String statement(int time) {
+//     time = 15;
+//     String statement="";
+   
+//     try {
+       
+// if (time < 12) {
+// statement= "오전입니다";
+// } else if (time < 18) {
+//     statement="오후입니다";
+//     } else {
+//         statement="밤입니다";
+//         }
+
+//     } catch (Exception e) {
+//         // TODO: handle exception
+//     }
+//     return statement;
+// }  
+
+// public static void main(String[] args) {
+// try {int time = 15;
+    
+//     MethodsIfs methodifs = new MethodsIfs();
+//     String returnresult = methodifs.statement(time);
+//     System.out.println(returnresult);
+
+// } catch (Exception e) {
+//     // TODO: handle exception
+// }
+// //return 0;
+// }
+
+// }
