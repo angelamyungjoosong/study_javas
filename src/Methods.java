08ec6fa4abbbf3732afe7bd4fec552ba19db2ce2
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-30 17:31:46 +0900

    done

--- src/Methods.java
+++ src/Methods.java
@@ -54,6 +54,9 @@ public class Methods {
     // {
     // //body
     // }
+ 
+        
+    
     public int minus(int first, int second){
         int result = 0;
         try {
@@ -72,23 +75,26 @@ public class Methods {
         }
         return result ;
     }
-    
-    public int main(int first, int second) {
+
+
+
+     public static void main(String[] args) {
+
         // - 인스턴스는 1회
         // - 초기 값은 2를 넘지 않음.
         // - main 결과값은 17
-        int result = 0;
-            try {first = 2;
-                second = 1;
+        
+            try { int first = 2;
+                 int  second = 1;
                 Methods methods = new Methods();
-                result = methods.add(first, first);
-                result = methods.add(first, first);
-                result = methods.add(first, first);
-                result = methods.add(first, second);
+                first = methods.add(first, first);
+                first = methods.add(first, first);
+                first = methods.add(first, first);
+                first = methods.add(first, second);
+                
     
         } catch (Exception e) {
             // TODO: handle exception
         }
-        return result;
-    }
-}
+        //return 0;
+    }}
