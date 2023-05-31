6b785ab481c72d14bdb28d3df3533c3ff4529fba
estskyway <estskyway@gmail.com>
2023-05-31 12:17:33 +0900

    5/31 임거산 입니다.

---
+++ src/GeosanLim.java
@@ -0,0 +1,10 @@
+public class GeosanLim {
+    public static void main(String[] args) {
+        try {
+            System.out.println("임거산 입니다.");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
