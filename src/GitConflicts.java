cc49b7f534c35acb20c51aa6d11d4102928e35dd
angelamyungjoosong <angelaforever122@gmail.com>
2023-05-31 11:47:32 +0900

    a

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,5 +1,5 @@
 public class GitConflicts {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try {
             int first =0;
             first = first + 1;
@@ -10,8 +10,5 @@ public class GitConflicts {
         }
         //return 0;
     }
-<<<<<<< HEAD
-=======
     
->>>>>>> 3479c6f339aaca8c452c49007aff409720ac2c17
 }
