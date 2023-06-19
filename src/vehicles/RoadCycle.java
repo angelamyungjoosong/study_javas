55d70382972ba6b3a1c570e57a4ad73b62a244c8
aabchyein <abcqaz369@naver.com>
2023-06-19 15:51:27 +0900

    수정

--- src/vehicles/RoadCycle.java
+++ src/vehicles/RoadCycle.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class RoadCycle extends Bicycle {
     // 퀘스트
-    // -제품명, 색상, 가격이 초기화
+    // -제품명, 색상, 가격 정보는 인스턴스 시 변수 셋팅
     // -toString()을 통해 제품명, 색상, 가격 출력
     // -상속은 Bicycle.java
     // -동작 확인은 Inheritances.java
